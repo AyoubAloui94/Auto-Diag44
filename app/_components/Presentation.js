0fdf8ff227178410892c781c839d213283da4eff
KaneWatson <webitgenius@gmail.com>
2024-06-25 16:22:30 +0200

    improve homepage presentation section

--- app/_components/Presentation.js
+++ app/_components/Presentation.js
@@ -6,14 +6,17 @@ import ActionButton from "./ActionButton"
 function Presentation() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-16 mb-16 px-6 max-w-[30rem] lg:max-w-[90rem] mx-auto">
-      <div className="flex flex-col gap-3 lg:justify-between items-start">
-        <h1 className="text-3xl">Diagnostic complet pour plus de 140 marques de voitures</h1>
-        <p className="text-justify">Notre service de diagnostic complet couvre plus de 140 marques de voitures, vous garantissant une analyse précise et fiable de votre véhicule. Faites confiance à notre expertise pour identifier et résoudre rapidement tous vos problèmes automobile.</p>
+      <div className="grid grid-cols-2 lg:grid-cols-1 gap-2">
+        <Image src={img1} width={"auto"} height={"auto"} alt="mercedes hands on stirring wheel" className="object-cover object-top rounded-2xl aspect-square lg:aspect-auto lg:h-96" />
+        <Image src={img2} width={"auto"} height={"auto"} alt="mercedes sports car" className="object-cover rounded-2xl object-top aspect-square lg:hidden" />
+      </div>
+      <div className="flex flex-col gap-3 lg:justify-between items-center">
+        <h1 className="text-3xl text-center">Diagnostic complet pour plus de 140 marques de voitures</h1>
+        <p className="text-center">Notre service de diagnostic complet couvre plus de 140 marques de voitures, vous garantissant une analyse précise et fiable de votre véhicule. Faites confiance à notre expertise pour identifier et résoudre rapidement tous vos problèmes automobile.</p>
 
         <ActionButton>Demandez un devis</ActionButton>
       </div>
-      <Image src={img1} width={"auto"} height={"auto"} alt="mercedes hands on stirring wheel" className="object-cover object-top rounded-lg h-96" />
-      <Image src={img2} width={"auto"} height={"auto"} alt="mercedes sports car" className="object-cover rounded-lg object-top h-96" />
+      <Image src={img2} width={"auto"} height={"auto"} alt="mercedes sports car" className="object-cover hidden lg:block rounded-2xl object-top h-96" />
     </div>
   )
 }
